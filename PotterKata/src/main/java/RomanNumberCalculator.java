18933c31d2266278d3327a1a718786e01ae61f8e
Inna Pirina <inna.pirina@cas.de>
2020-07-15 18:38:57 +0200

    add initial classes for roman calculator

---
+++ PotterKata/src/main/java/RomanNumberCalculator.java
@@ -0,0 +1,6 @@
+/**
+ * @author Inna Pirina (<a href="mailto:Inna.Pirina@cas.de">Inna.Pirina@cas.de</a>)
+ * @since 15.07.2020
+ */
+public class RomanNumberCalculator {
+}
---
+++ PotterKata/src/test/java/RomanNumberCalculatorTest.java
@@ -0,0 +1,10 @@
+import jdk.internal.jline.internal.TestAccessible;
+
+/**
+ * @author Inna Pirina (<a href="mailto:Inna.Pirina@cas.de">Inna.Pirina@cas.de</a>)
+ * @since 15.07.2020
+ */
+
+public class RomanNumberCalculatorTest {
+
+}
